faea143a1e9deb6f3e344511707bb6cc6fb3ec55
Olivia Ostwald <Olivia.Ostwald@noaa.gov>
2024-06-13 17:59:43 +0000

    Removing cron scripts (WCOSS2 prod switch)

--- cron_jobs/test.sh
+++
@@ -1,1 +0,0 @@
-echo /lfs/h1/ops/prod/com/ens_tracker/v1.3/gefs.$(date "+%Y%m%d")/06/genesis/
--- cron_jobs/ukmetdata.sh
+++
@@ -1,7 +0,0 @@
-cd /lfs/h2/emc/vpppg/noscrub/olivia.ostwald/Data/Year2023/ukmet
-
-cp -r /lfs/h1/ops/dev/dcom/$(date -d "-1 day" "+%Y%m%d")/wtxtbul/ukmet_hurr/* .
-echo "Copying $(date -d "-1 day" "+%Y%m%d") UKMET files"
-#cp -r /lfs/h1/ops/dev/dcom/$(date "+%Y%m%d")/wtxtbul/ukmet_hurr/* .
-#echo "Copying $(date -d "+%Y%m%d") UKMET files"
-
