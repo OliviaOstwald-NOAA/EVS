eba22599cbff3e7d1c2128ddce77d87dcba59aa9
Olivia Ostwald <Olivia.Ostwald@noaa.gov>
2024-07-08 18:13:33 +0000

    HAFSv2 runs for AL02 Beryl

--- scripts/plots/hurricane/exevs_hurricane_regional_tropcyc_plots.sh
+++ scripts/plots/hurricane/exevs_hurricane_regional_tropcyc_plots.sh
@@ -3,9 +3,11 @@ set -x
 export PS4=' + exevs_hurricane_regional_tropcyc_plots.sh line $LINENO: '
 
 export stormYear=${YYYY}
-export basinlist="al ep"
-export numlist="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 \ # HAFS became operational on June 27, 2023. 
-	        21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40"  # Any runs for AL01, AL02, AL03 are not included in input.
+export basinlist="al"
+#export basinlist="al ep"
+export numlist="02"
+#export numlist="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 \  
+#	        21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40"  
 
 for bas in $basinlist; do
 ### bas do loop start
@@ -74,8 +76,8 @@ export PLOTDATA=${STORMroot}
 #export RUN="tropcyc"
 export img_quality="low"
 export fhr_list="0,6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96,102,108,114,120,126"
-export model_tmp_atcf_name_list="MD01,MD02,MD03,MD04,MD05,MD06"
-export model_plot_name_list="HFSA,HFSB,HWRF,HMON,GFS,CTCX"
+export model_tmp_atcf_name_list="MD01,MD02,MD03,MD04,MD05,MD06,MD07"
+export model_plot_name_list="HFSA,HFSB,HWRF,HMON,GFS,HV2A,HV2B"
 export plot_CI_bars="NO"
 export under="_"
 export tc_name=${stbasin}${under}${stormYear}${under}${stormName}
@@ -141,8 +143,8 @@ export PLOTDATA=${metTCcomout}
 #export RUN="tropcyc"
 export img_quality="low"
 export fhr_list="0,6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96,102,108,114,120,126"
-export model_tmp_atcf_name_list="MD01,MD02,MD03,MD04,MD05,MD06"
-export model_plot_name_list="HFSA,HFSB,HWRF,HMON,GFS,CTCX"
+export model_tmp_atcf_name_list="MD01,MD02,MD03,MD04,MD05,MD06,MD07"
+export model_plot_name_list="HFSA,HFSB,HWRF,HMON,GFS,HV2A,HV2B"
 export plot_CI_bars="NO"
 export stormNameB=Basin
 export tc_name=${stbasin}${under}${stormYear}${under}${stormNameB}
