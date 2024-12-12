e9ecca20704549257044f3bf6dff6eacf798a7ed
Olivia Ostwald <Olivia.Ostwald@noaa.gov>
2024-12-12 21:57:55 +0000

    verif for Sara (AL19)

--- scripts/plots/hurricane/exevs_hurricane_regional_tropcyc_plots.sh
+++ scripts/plots/hurricane/exevs_hurricane_regional_tropcyc_plots.sh
@@ -4,7 +4,7 @@ export PS4=' + exevs_hurricane_regional_tropcyc_plots.sh line $LINENO: '
 
 export stormYear=${YYYY}
 export basinlist="al ep"
-export numlist="04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20" 
+export numlist="04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19" 
 #export numlist="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 \  
 #	        21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40"  
 
