881045ffb3861361aa414fb9c782770b2e32f05f
Olivia Ostwald <Olivia.Ostwald@noaa.gov>
2025-06-18 17:54:46 +0000

    Runs for EP05 (mid-storm)

--- scripts/plots/hurricane/exevs_hurricane_global_det_tropcyc_plots.sh
+++ scripts/plots/hurricane/exevs_hurricane_global_det_tropcyc_plots.sh
@@ -7,7 +7,7 @@ export stormYear=${YYYY}
 export basinlist="ep"
 #export numlist="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 \
 #	        21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40"
-export numlist="02 03"
+export numlist="02 03 04 05"
 
 for bas in $basinlist; do
 ### bas do loop start
@@ -76,10 +76,10 @@ export PLOTDATA=${STORMroot}
 #export RUN="tropcyc"
 export img_quality="low"
 export fhr_list="0,12,24,36,48,60,72,84,96,108,120,132,144,156,168"
-#export model_tmp_atcf_name_list="MD01,MD02,MD03,MD04"
-export model_tmp_atcf_name_list="MD01,MD02,MD03"
-#export model_plot_name_list="GFS,ECMWF,CMC,UKM"
-export model_plot_name_list="GFS,ECMWF,CMC"
+export model_tmp_atcf_name_list="MD01,MD02,MD03,MD04"
+#export model_tmp_atcf_name_list="MD01,MD02,MD03"
+export model_plot_name_list="GFS,ECMWF,CMC,UKM"
+#export model_plot_name_list="GFS,ECMWF,CMC"
 export plot_CI_bars="NO"
 export under="_"
 export tc_name=${stbasin}${under}${stormYear}${under}${stormName}
@@ -143,10 +143,10 @@ export PLOTDATA=${metTCcomout}
 #export RUN="tropcyc"
 export img_quality="low"
 export fhr_list="0,12,24,36,48,60,72,84,96,108,120,132,144,156,168"
-#export model_tmp_atcf_name_list="MD01,MD02,MD03,MD04"
-export model_tmp_atcf_name_list="MD01,MD02,MD03"
-#export model_plot_name_list="GFS,ECMWF,CMC,UKM"
-export model_plot_name_list="GFS,ECMWF,CMC"
+export model_tmp_atcf_name_list="MD01,MD02,MD03,MD04"
+#export model_tmp_atcf_name_list="MD01,MD02,MD03"
+export model_plot_name_list="GFS,ECMWF,CMC,UKM"
+#export model_plot_name_list="GFS,ECMWF,CMC"
 export plot_CI_bars="NO"
 export stormNameB=Basin
 export tc_name=${stbasin}${under}${stormYear}${under}${stormNameB}
