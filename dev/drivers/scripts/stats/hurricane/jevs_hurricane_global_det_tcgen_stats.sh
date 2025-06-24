2c2d4295cf9f5a2f85b0bb4cf49ca1af8a4c728f
Olivia Ostwald <Olivia.Ostwald@noaa.gov>
2025-06-24 22:59:32 +0000

    tests for controlled genesis experiment

--- dev/drivers/scripts/stats/hurricane/jevs_hurricane_global_det_tcgen_stats.sh
+++ dev/drivers/scripts/stats/hurricane/jevs_hurricane_global_det_tcgen_stats.sh
@@ -36,7 +36,7 @@ source ${HOMEevs}/dev/modulefiles/${COMPONENT}/${COMPONENT}_${STEP}.sh
 export PDY=20241231
 
 #Define the directories of your TC genesis data and A/Bdeck files
-export COMINgenesis=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/TCgen
+export COMINgenesis=/lfs/h2/emc/vpppg/noscrub/$USER/Data/Year2024/data-comparison/test5
 export COMINadeckNHC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/adeck
 export COMINbdeckNHC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/bdeck
 export COMINadeckJTWC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/adeck
--- scripts/plots/hurricane/exevs_hurricane_global_det_tcgen_plots.sh
+++ scripts/plots/hurricane/exevs_hurricane_global_det_tcgen_plots.sh
@@ -6,9 +6,9 @@ export PS4=' + exevs_hurricane_global_det_tcgen_plots.sh line $LINENO: '
 export cartopyDataDir=${cartopyDataDir:-/apps/ops/prod/data/cartopy}
 
 export YEAR=${YYYY}
-export TCGENdays="TC Genesis (05/01/${YEAR}-11/30/${YEAR})"
-export basinlist="al ep wp"
-export modellist="gfs ecmwf cmc"
+export TCGENdays="TC Genesis (09/01/${YEAR}-09/30/${YEAR}) {Test #5}"
+export basinlist="al"
+export modellist="gfs"
 
 noaa_logo() {
   TargetImageName=$1
