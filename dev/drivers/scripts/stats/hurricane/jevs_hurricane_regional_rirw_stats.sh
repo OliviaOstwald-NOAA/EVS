43718b836b6ee33bfdb806eb515259eaa782b850
Olivia Ostwald <Olivia.Ostwald@noaa.gov>
2025-08-18 19:12:07 +0000

    adding RIRW scripts from previous dev work in 2022

---
+++ dev/drivers/scripts/stats/hurricane/jevs_hurricane_regional_rirw_stats.sh
@@ -0,0 +1,49 @@
+#PBS -S /bin/bash
+#PBS -N jevs_hurricane_regional_rirw_stats
+#PBS -j oe
+#PBS -A VERF-DEV
+#PBS -q dev
+#PBS -l select=1:ncpus=1:mem=4GB
+##PBS -l place=vscatter:exclhost,select=1:ncpus=128:ompthreads=1
+#PBS -l walltime=01:00:00
+#PBS -l debug=true
+
+set -x
+
+export HOMEevs=/lfs/h2/emc/vpppg/noscrub/$USER/EVS
+source ${HOMEevs}/versions/run.ver
+
+evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
+
+export NET=evs
+export COMPONENT=hurricane
+export RUN=regional
+export STEP=stats
+export VERIF_CASE=rirw
+export envir=dev
+export cyc=00
+export job=jevs_${COMPONENT}_${RUN}_${VERIF_CASE}_${STEP}_${cyc}
+export jobid=$job.${PBS_JOBID:-$$}
+
+############################################################
+# Load modules
+############################################################
+module reset
+source ${HOMEevs}/dev/modulefiles/${COMPONENT}/${COMPONENT}_${STEP}.sh 
+
+export PDY=20241231 
+#Set PDY to override setpdy.sh called in the j-jobs
+
+#Define TC-vital file, TC track file and the directory for Bdeck files
+export COMINvit=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/syndat_tcvitals.2024
+export COMINtrack=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/tracks.atcfunix.24
+export COMINbdeckNHC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/bdeck
+export COMINbdeckJTWC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/bdeck
+
+export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
+export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d
+export KEEPDATA=NO
+
+# CALL executable job script here
+$HOMEevs/jobs/JEVS_HURRICANE_STATS
+
