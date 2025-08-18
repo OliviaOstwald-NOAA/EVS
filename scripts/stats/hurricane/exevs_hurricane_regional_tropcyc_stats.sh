74302e9f0e56322ea214da964769dc1de8d5a55b
Olivia Ostwald <Olivia.Ostwald@noaa.gov>
2025-08-18 17:18:26 +0000

    test with consensus model runs for 2025

--- scripts/stats/hurricane/exevs_hurricane_regional_tropcyc_stats.sh
+++ scripts/stats/hurricane/exevs_hurricane_regional_tropcyc_stats.sh
@@ -93,14 +93,16 @@ grep "03, HWRF" tracks.atcfunix.${YY25}_${stormBasin}${stormNumber} >> a${stormB
 grep "03, HMON" tracks.atcfunix.${YY25}_${stormBasin}${stormNumber} >> a${stormBasin}${stormNumber}${stormYear}.dat
 grep "03, AVNO" tracks.atcfunix.${YY25}_${stormBasin}${stormNumber} >> a${stormBasin}${stormNumber}${stormYear}.dat
 grep "03, CTCX" tracks.atcfunix.${YY25}_${stormBasin}${stormNumber} >> a${stormBasin}${stormNumber}${stormYear}.dat
+#grep "03, HCCA" tracks.atcfunix.${YY25}_${stormBasin}${stormNumber} >> a${stormBasin}${stormNumber}${stormYear}.dat
 sed -i 's/03, HFSA/03, MD01/' a${stormBasin}${stormNumber}${stormYear}.dat
 sed -i 's/03, HFSB/03, MD02/' a${stormBasin}${stormNumber}${stormYear}.dat
 sed -i 's/03, HWRF/03, MD03/' a${stormBasin}${stormNumber}${stormYear}.dat
 sed -i 's/03, HMON/03, MD04/' a${stormBasin}${stormNumber}${stormYear}.dat
 sed -i 's/03, AVNO/03, MD05/' a${stormBasin}${stormNumber}${stormYear}.dat
 sed -i 's/03, CTCX/03, MD06/' a${stormBasin}${stormNumber}${stormYear}.dat
+#sed -i 's/03, HCCA/03, MD07/' a${stormBasin}${stormNumber}${stormYear}.dat
 export Model_List="MD01,MD02,MD03,MD04,MD05,MD06"
-#export Model_List="MD01,MD02,MD03,MD04,MD05"
+#export Model_List="MD01,MD02,MD03,MD04,MD05,MD06,MD07"
 
 #---get the $startdate, $enddate[YYMMDDHH] from the best track file  
 echo $(head -n 1 ${bdeckfile}) > head.txt
